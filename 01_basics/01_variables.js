9318e2f4b946c36afb217866a1b86cb8521855db
Dipendra Chaudhary <ddipendrac@gmail.com>
2024-12-19 12:56:14 +0000

    var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123444
+let accountEmail = "dipendra@google.com"
+var accountPassword = "23456"
+//accountId = 2
+console.log(accountId);
+
+/*
+ Do not use var because of issue in scope
+*/
