cc1bff1773aa9a798b93d7fbd5b09bc13e0b3c6e
Dipendra Chaudhary <ddipendrac@gmail.com>
2025-01-02 15:42:08 +0000

    object and array in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,4 +19,38 @@ function loginUserMessage(username = "Sam") {
     return `${username} just logged in` 
 }
 
-console.log(loginUserMessage("Dipendra"));
+// console.log(loginUserMessage("Dipendra"));
+
+// function calculateCartPrice(val1, val2, ...num1) {  // Here, first num goes to val1, sec goes to val2 then other to  ...rest operator
+//     return num1
+// }
+
+function calculateCartPrice(...num1) {  // Here ... is rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 500));
+
+const user = {
+    userName: "Dipendra",
+    email: "dipendra@google.com"
+}
+
+function handleObject(anyObject) {
+    console.log(`User name is ${anyObject.userName} and email is ${anyObject.email}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    userName: "Sam",
+    email: "sam@google.com"
+})
+
+const myArray = [200, 300, 400,100]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myArray))
