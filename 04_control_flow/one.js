f1bbc404ea3a64046083ee06437ad5902f27cf35
Dipendra Chaudhary <ddipendrac@gmail.com>
2025-01-05 13:27:33 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,42 @@
+// if
+// <, >, <=, >=, ==, ===, !=, !==
+
+// if (true) {
+    
+// }
+
+const isUserLoggedIn = true
+// const termperature = 45
+
+// if (termperature < 50) {
+//     console.log("Less than 50");
+// } else {
+//     console.log("Greater than 50");
+// }
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+    
+}
