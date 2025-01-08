89c0676e7799f69eeec3e93981c362a7595d5049
Dipendra Chaudhary <ddipendrac@gmail.com>
2025-01-08 13:47:08 +0000

    forEach in object

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,1 +1,39 @@
-con
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// forEach looks each value of array and provides to function as a parameter
+// coding.forEach(function(val){
+//     console.log(val);
+// })
+
+coding.forEach( (item) => {
+    // console.log(item)
+})
+
+function printMe(item) {
+    // console.log(item);
+}
+
+// coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+})
+
+const myCoding = [
+    {
+        languageName: "javaScript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageFileName)
+})
